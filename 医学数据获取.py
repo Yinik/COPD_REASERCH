89639bf3137527235804473b1886b6b1f4e500c8
橙汁儿 <2952602670@qq.com>
2026-05-22 12:43:10 +0800

    删除路径硬编码的旧版脚本，准备统一迁移到config.py

--- 医学数据获取.py
+++
@@ -1,4 +0,0 @@
-# 用之前给你的BioPython代码，搜中文糖尿病文献
-from Bio import Entrez
-Entrez.email = "你的邮箱@qq.com"
-handle = Entrez.esearch(db="pubmed", term="diabetes[Title/Abstract] AND chinese[Language]", retmax=50)
